5f6160b54386c0cfd30181c0decb3fff661019e5
bhishmdt <bhishmdt@gmail.com>
2021-05-12 23:21:07 +0530

    Add new

---
+++ findSubsets.py
@@ -0,0 +1,51 @@
+"""
+Find subsets of a given array
+"""
+from itertools import combinations
+
+def findSubsets(A):
+    """
+    Recursive solution
+    """
+    result = []
+    helper(A, [], result)
+    return result
+
+def helper(A, path, result):
+    result.append(path)
+    for i in range(len(A)):
+        helper(A[i + 1:], path + [A[i]], result)
+
+def findSubsets2(A):
+    """
+    Iterative solution
+    """
+    result = [[]]
+    for e in A:
+        for i in range(len(result)):
+            result.append(result[i] + [e])
+    return result
+
+def findSubsets3(A):
+    """
+    Using itertools.combinations
+    """
+    result = []
+    for i in range(len(A)+1):
+        for s in combinations(A, i):
+            result.append(list(s))
+
+    return result
+
+if __name__ == '__main__':
+    A = [2, 1, 4, 5]
+    print(findSubsets(A))
+    # [[], [2], [2, 1], [2, 1, 4], [2, 1, 4, 5], [2, 1, 5], [2, 4], [2, 4, 5], [2, 5], [1], [1, 4], [1, 4, 5], [1, 5], [4], [4, 5], [5]]
+
+    A = [2, 1, 4, 5]
+    print(findSubsets2(A))
+    #  [[], [2], [1], [2, 1], [4], [2, 4], [1, 4], [2, 1, 4], [5], [2, 5], [1, 5], [2, 1, 5], [4, 5], [2, 4, 5], [1, 4, 5], [2, 1, 4, 5]]
+
+    A = [2, 1, 4, 5]
+    print(findSubsets3(A))
+    # [[], [2], [1], [4], [5], [2, 1], [2, 4], [2, 5], [1, 4], [1, 5], [4, 5], [2, 1, 4], [2, 1, 5], [2, 4, 5], [1, 4, 5], [2, 1, 4, 5]]
