ddc0a02f44fc06a5a48884eeb4d16a3910e9168b
bhishmdt <bhishmdt@gmail.com>
2021-05-18 00:08:36 +0530

    Add new

--- allWords.sh
+++ allWords.sh
@@ -4,4 +4,4 @@
 echo "Total occurrences"
 find ~/My_Django_stuff/inventory/. -type f -exec cat {} + | wc -w
 echo "Total words"
-find ~/My_Django_stuff/inventory/. -type f -exec cat {} + | tr ' ' '\n' |sort| uniq -c | wc -l
+find ~/My_Django_stuff/inventory/. -type f -exec cat {} + | tr ' ' '\n' | sort -u | wc -l
